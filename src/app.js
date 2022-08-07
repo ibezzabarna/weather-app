33191f4b6cfb4be19d0ce678dfcd5651371d0695
Ilona <ibezzabarna@gmail.com>
2022-08-07 18:16:12 +0300

    added temperature conversion

--- src/app.js
+++ src/app.js
@@ -25,14 +25,39 @@ date.innerHTML = formatDate(new Date());
 function showWeather(response) {
   document.querySelector("#description").innerHTML = response.data.weather[0].description;
   document.querySelector("#degrees").innerHTML = Math.round(response.data.main.temp);
+  temperatureCelsius = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = response.data.wind.speed;
   document.querySelector("#last-update").innerHTML = formatDate(new Date());
   let iconElement = document.querySelector("#icon");
-  console.log(iconElement)
   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description)
 }
+
+function convertToFahrenheit(celsius) {
+  let temperatureFahrenheit = celsius * 9 / 5 + 32;
+  return Math.round(temperatureFahrenheit);
+}
+let temperatureCelsius = 24
+
+function displayTemperatureCelsius(event) {
+  event.preventDefault();
+  event.target.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  document.querySelector("#degrees").innerHTML = temperatureCelsius;
+}
+let celsiusLink= document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayTemperatureCelsius);
+
+function displayTemperatureFahrenheit(event) {
+  event.preventDefault();
+  event.target.classList.add("active");
+  celsiusLink.classList.remove("active");
+  let temperatureFahrenheit = convertToFahrenheit(temperatureCelsius);
+  document.querySelector("#degrees").innerHTML = temperatureFahrenheit;
+}
+let fahrenheitLink= document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", displayTemperatureFahrenheit);
 
 function updateWeatherReport(event) {
   event.preventDefault();
